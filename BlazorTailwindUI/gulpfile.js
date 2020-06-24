f53635d3c3d1dca871f88ad6bb522f98883cfbe0
Fabien Dehopré <fabien.dehopre@fluxys.com>
2020-06-24 16:12:17 +0200

    install minimal npm deps for tailwindcss

---
+++ BlazorTailwindUI/gulpfile.js
@@ -0,0 +1,12 @@
+﻿const gulp = require('gulp');
+
+gulp.task('css', () => {
+  const postcss = require('gulp-postcss');
+  return gulp.src('./Styles/styles.css')
+    .pipe(postcss([
+      require('precss'),
+      require('tailwindcss'),
+      require('autoprefixer'),
+    ]))
+    .pipe(gulp.dest('./wwwroot/css/'));
+});
