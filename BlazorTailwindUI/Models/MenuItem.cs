7bd387c04eab64d3d11e59c0a17de0036a07e10d
Fabien Dehopré <fabien@dehopre.com>
2020-07-03 08:24:13 +0200

    WIP: moving menu and menu items into their component

---
+++ BlazorTailwindUI/Models/MenuItem.cs
@@ -0,0 +1,9 @@
+namespace BlazorTailwindUI.Models
+{
+    public class MenuItem
+    {
+        public string Label { get; set; }
+        public string Url { get; set; }
+        public string Icon { get; set; }
+    }
+}
---
+++ BlazorTailwindUI/Shared/NavMenu.razor.cs
@@ -0,0 +1,10 @@
+namespace BlazorTailwindUI.Shared
+{
+    using Microsoft.AspNetCore.Components;
+
+    public partial class NavMenu
+    {
+        [Parameter]
+        public bool IsDesktop { get; set; }
+    }
+}
